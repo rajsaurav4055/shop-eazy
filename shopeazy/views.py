f75bc37fcc72859f509063f8c6cc840052163a77
Saurav Raj <raj.sauravwq@gmail.com>
2022-10-30 17:20:17 -0400

    Adding SignIn Page frontend

--- shopeazy/views.py
+++ shopeazy/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def home(request):
@@ -10,11 +11,11 @@ def home(request):
 def signup(request):
     if request.method == "POST":
         username = request.POST['username']
-        firstname = request.POST['firstname']
-        lastname = request.POST['lastname']
-        email = request.POST['email']
-        password = request.POST['password']
-        confirmpassword = request.POST['confirmpassword']
+        firstname = request.POST.get('firstname',False)
+        lastname = request.POST.get('lastname', False)
+        email = request.POST.get('email', False)
+        password = request.POST.get('password', False)
+        confirmpassword = request.POST.get('confirmpassword',False)
 
         myuser = User.objects.create_user(username, email, password)
         myuser.first_name = firstname
@@ -29,7 +30,24 @@ def signup(request):
     return render(request, "shopeazy/signup.html")
 
 def signin(request):
+    if request.method == 'POST':
+        username = request.POST.get('username',False)
+        password = request.POST.get('password', False)
+
+        user = authenticate(username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            firstname = user.first_name
+            return render(request, "shopeazy/index.html", {'firstname' : firstname})
+
+        else:
+            messages.error(request, "Bad Credentials!")
+            return redirect('home')
+
     return render(request, "shopeazy/signin.html")
 
 def signout(request):
-    pass            
+    logout(request)
+    messages.success(request, "Logged Out Successfully!")
+    return redirect('home')            
