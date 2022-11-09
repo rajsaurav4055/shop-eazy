a6fe272067b7dc128027c6136724f26c5ef16b5d
dhillonajender <dhillonajender@gmail.com>
2022-11-08 19:51:22 -0500

    updating view.py and url.py file

--- frontendshopeazy/shopeazy/urls.py
+++ frontendshopeazy/shopeazy/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('signup', views.signup, name="signup"),
     path('signin', views.signin, name="signin"),
+    path('homepage', views.homepage, name="homepage"),
+    path('order', views.order, name="order"),
+    path('cart', views.cart, name="cart"),
     path('signout', views.signout, name="signout"),
 ]
