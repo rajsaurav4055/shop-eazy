602a0d083ba92f9ed97802d035be0f887b304a4f
Gurjot Singh <gurjotsingh@Gurjots-MacBook-Air.local>
2022-12-01 21:32:19 -0500

    Home page change, db updated, add images

--- frontendshopeazy_project/frontendshopeazy/settings.py
+++ frontendshopeazy_project/frontendshopeazy/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'full_stack1',  
         'USER': 'root',  
-        'PASSWORD': 'Ajender@950',  
+        'PASSWORD': 'Alaskahusky@1234',  
         'HOST': '127.0.0.1',  
         'PORT': '3306',  
         'OPTIONS': {  
